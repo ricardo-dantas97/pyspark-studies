d0fc2de4474468fa9701fc44c6f099bea9899837
Ricardo Dantas <ricarddodantas@outlook.com>
2023-11-10 16:53:07 -0300

    Alterando arquivo com funções mais usadas

--- 00-utils.py
+++ 00-utils.py
@@ -1,18 +1,27 @@
 # Databricks notebook source
 from pyspark.sql.types import *
 
-data = [
+# COMMAND ----------
+
+data_df = [
   {'id': 1, 'name': 'Ricardo', 'salary': 12000.0, 'gender': 'm', 'department_id': 2, 'manager_id': 0},
   {'id': 2, 'name': 'Jordana', 'salary': 15000.0, 'gender': 'f', 'department_id': 3, 'manager_id': 1},
   {'id': 7, 'name': 'Beatriz', 'salary': 13000.0, 'gender': 'f', 'department_id': 3, 'manager_id': 1},
-  {'id': 3, 'name': 'Lucas', 'salary': 5000.0, None: 'm', 'department_id': 2, 'manager_id': 2},
+  {'id': 3, 'name': 'Lucas', 'salary': 5000.0, 'gender': 'm', 'department_id': 2, 'manager_id': 2},
   {'id': 4, 'name': 'Rodrigo', 'salary': 4000.0, 'gender': 'm', 'department_id': 1, 'manager_id': 0},
   {'id': 6, 'name': 'Rodrigo', 'salary': 4000.0, 'gender': 'm', 'department_id': 1, 'manager_id': 1},
   {'id': 8, 'name': 'Lais', 'salary': 8000.0, 'gender': 'm', 'department_id': 3, 'manager_id': 1},
   {'id': 5, 'name': 'João', 'salary': 25000.0, 'gender': 'm', 'department_id': 4, 'manager_id': 2},
   {'id': 9, 'name': 'Gabriel', 'salary': 25000.0, 'gender': 'm', 'department_id': 7, 'manager_id': 4},
-  {'id': None, 'name': None, 'salary': 25000.0, 'gender': None, 'department_id': None, 'manager_id': None},
+  {'id': None, 'name': None, 'salary': 25000.0, 'gender': None, 'department_id': None, 'manager_id': None}
 ]
+
+schema = StructType().add(field='id', data_type=IntegerType(), nullable=True) \
+                       .add(field='name', data_type=StringType(), nullable=True) \
+                       .add(field='salary', data_type=DoubleType(), nullable=True) \
+                       .add(field='gender', data_type=StringType(), nullable=True) \
+                       .add(field='department_id', data_type=StringType(), nullable=True) \
+                       .add(field='manager_id', data_type=StringType(), nullable=True) 
 
 data_dup = [
   {'id': 1, 'name': 'Ricardo', 'salary': 12000.0, 'gender': 'm'},
@@ -26,19 +35,17 @@ data_dup = [
   {'id': 5, 'name': 'João', 'salary': 25000.0, 'gender': 'm'},
 ]
 
-def create_standard_df(type_df='normal'):
-  schema = StructType().add(field='id', data_type=IntegerType()) \
-                       .add(field='name', data_type=StringType()) \
-                       .add(field='salary', data_type=DoubleType()) \
-                       .add(field='gender', data_type=StringType()) \
-                       .add(field='department_id', data_type=StringType()) \
-                       .add(field='manager_id', data_type=StringType()) 
-                        
+schema_dup = StructType().add(field='id', data_type=IntegerType(), nullable=True) \
+                            .add(field='name', data_type=StringType(), nullable=True) \
+                            .add(field='salary', data_type=DoubleType(), nullable=True) \
+                            .add(field='gender', data_type=StringType(), nullable=True)
 
+
+def create_standard_df(type_df='normal'):
   if type_df == 'dup':
-    df = spark.createDataFrame(data_dup, schema=schema)
+    df = spark.createDataFrame(data_dup, schema=schema_dup)
   else:
-    df = spark.createDataFrame(data, schema=schema)
+    df = spark.createDataFrame(data_df, schema=schema)
 
   return df
 
@@ -58,3 +65,11 @@ def create_departments_df():
 
   df = spark.createDataFrame(departments, schema)
   return df
+
+# COMMAND ----------
+
+data_rdd = [(1, 'Ricardo'), (2, 'Jordana')]
+
+def create_basic_rdd(data=data_rdd):
+  rdd = spark.sparkContext.parallelize(data)
+  return rdd
