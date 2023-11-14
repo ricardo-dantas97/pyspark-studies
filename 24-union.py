07b5d6b2c560f674ef1c572d917a5a0b36fa577c
Ricardo Dantas <ricarddodantas@outlook.com>
2023-11-14 17:29:36 -0300

    Atualizando arquivos duplicados

--- 24-union.py
+++ 24-union.py
@@ -1,4 +1,22 @@
 # Databricks notebook source
+# MAGIC %run "/yt-course-india/00-utils"
+
+# COMMAND ----------
+
+df = create_standard_df()
+df2 = create_standard_df(type_df='dup')
+
+# COMMAND ----------
+
+# Union function will append one df to another, increasing the quantity of lines
+display(df.union(df2))
+
+# COMMAND ----------
+
+# If we want to remove duplicates, we can use distinct function together
+new_df = df.union(df2).distinct()
+display(new_df.sort(df['id']))
+
 # MAGIC %md
 # MAGIC Union by name function allows us to union two dfs even if they don't have the same schema  
 # MAGIC Columns that don't fit, get null values
